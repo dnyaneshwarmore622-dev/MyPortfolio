707708332ea57a2db5b975cfcded9db871c4cbfc
dnyaneshwarmore622-dev <dnyaneshwarmore622@gmail.com>
2026-01-12 16:22:36 +0530

    Add files via upload
    final update

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: [
+  "SDE with expertise in JavaScript & Python",
+  "Data Engineer using SQL, Spark & Airflow",
+  "Cloud Developer (AWS | GCP)",
+  "MERN Stack Developer",
+  "Big Data Pipeline Architect"
+],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
